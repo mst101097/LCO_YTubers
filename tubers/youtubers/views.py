222080e97666c0fe493caa20113203589980e4de
mst101097 <singh.1097.mohit@gmail.com>
2021-01-29 14:17:03 +0530

    Initial commit and submit commpleted Modules

---
+++ tubers/youtubers/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render , get_object_or_404
+from .models import Youtuber
+
+# Create your views here.
+def youtubers(request):
+    tubers = Youtuber.objects.order_by('-created_date')
+    data = {
+        'tubers':tubers,
+    }
+    return render(request,'youtubers/youtubers.html',data)
+
+
+def youtubers_detail(request , id):
+    tuber = get_object_or_404(Youtuber,pk = id)
+    data = {
+        'tuber':tuber
+    }
+    return render(request,'youtubers/youtuber_detail.html',data)
+
+def search(request):
+    pass
